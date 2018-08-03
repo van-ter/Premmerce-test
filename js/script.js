a4630b1212d95eea053f45d1167cce55a398e852
van-ter <ivantr1995@gmail.com>
2018-08-03 09:53:18 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  /*var mySwiper = new Swiper('#slider1', {
+    speed: 600,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+      nextEl: '#slider1 .swiper-button-next',
+      prevEl: '#slider1 .swiper-button-prev'
+    },
+    effect: 'fade',
+    pagination: {
+      el: '#slider1 .swiper-pagination',
+      clickable: true
+    }
+  });*/
+
+
+  /*var mySwiper3 = new Swiper('#slider3', {
+    speed: 600,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+      nextEl: '.slider3-wraper .swiper-button-next',
+      prevEl: '.slider3-wraper .swiper-button-prev'
+    }
+  });*/
+
+  /*$('.book').magnificPopup({
+    type: 'inline',
+    preloader: false
+  });*/
+
+  /*$('.btn-menu').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    callbacks: {
+      close: function() {
+        $('#menu').removeClass('mfp-hide');
+      }
+    }
+  });*/
+
+
+  
+});
